716f06462c2dde9840fbef8d1fd3e029bb68cd8d
Rahmot15 <mdrahmatulla666@gmail.com>
2025-05-23 20:30:53 +0600

    MyPlants item deleted

--- src/Pages/MyPlants.jsx
+++ src/Pages/MyPlants.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router";
 import { MdDelete, MdEdit } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const MyPlants = () => {
-    const plants = useLoaderData();
-    return (
-        <div>
+  const initialPlants = useLoaderData();
+  const [plants, setPlants] = useState(initialPlants);
+
+  const handleDelete = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/plants/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount) {
+              const remainingPlant = plants.filter(plant => plant._id !== id)
+              setPlants(remainingPlant)
+
+              Swal.fire({
+                title: "Deleted!",
+                text: "Plant has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
+
+  return (
+    <div>
       <div className="overflow-x-auto mt-12 w-11/12 mx-auto">
         <table className="table">
           {/* head */}
@@ -51,9 +96,16 @@ const MyPlants = () => {
                 <td> {plant.Category} </td>
                 <th>
                   <div className="card-actions  gap-3">
-            <button className="btn  btn-primary"><MdEdit size={20}/></button>
-            <button className="btn  btn-primary"><MdDelete size={20}/> </button>
-          </div>
+                    <button className="btn  btn-primary">
+                      <MdEdit size={20} />
+                    </button>
+                    <button
+                      onClick={() => handleDelete(plant._id)}
+                      className="btn  btn-primary"
+                    >
+                      <MdDelete size={20} />{" "}
+                    </button>
+                  </div>
                 </th>
               </tr>
             ))}
@@ -61,7 +113,7 @@ const MyPlants = () => {
         </table>
       </div>
     </div>
-    );
+  );
 };
 
 export default MyPlants;
